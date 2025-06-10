a8ebd1d608b9ce3e12724e17c03e891f16ec92c3
Viet Hung <viethung01234@gmail.com>
2025-06-11 06:59:09 +0700

    add error handling msg

--- app/src/main/java/com/example/myapplication/dto/ErrorResponse.java
+++ app/src/main/java/com/example/myapplication/dto/ErrorResponse.java
@@ -7,16 +7,34 @@ public class ErrorResponse {
     @SerializedName("error")
     private String error;
 
+    @SerializedName("title")
+    private String title;
+
 
     public ErrorResponse(String error) {
         this.error = error;
+
+    }
+
+    public ErrorResponse(String error, String title) {
+        this.error = error;
+        this.title = title;
     }
 
     public String getError() {
+
+        if (this.error == null || this.error.isEmpty()) {
+            return this.title;
+        }
+
         return error;
     }
 
     public void setError(String error) {
         this.error = error;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
